794832bce919ea93425364dfe4e784f85353f457
Breno Lemos <brenomiros@gmail.com>
2022-10-19 19:15:21 -0300

    Create the checkout success page

--- src/pages/checkout/[id]/success.tsx
+++ src/pages/checkout/[id]/success.tsx
@@ -1,13 +1,32 @@
-import { NextPage } from "next";
+import { GetStaticProps, GetStaticPaths, NextPage } from "next";
+import { http } from "../../../utils/https";
 
 const CheckoutSuccess: NextPage = () => {
-  return <div>
-    <h3>Congrats! Order #ID was submited.</h3>
+  return (
+    <div>
+      <h3>Congrats! Order #ID was submited.</h3>
 
-    <ul>
-      <li></li>
-    </ul>
-  </div>;
+      <ul>
+        <li></li>
+      </ul>
+    </div>
+  );
+};
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  return {
+    paths: [],
+    fallback: "blocking",
+  };
+};
+
+export const getStaticProps: GetStaticProps = async (context) => {
+  const { id } = context.params || {};
+  const { data: order } = await http.get(`orders/${id}`);
+
+  return {
+    props: {},
+  };
 };
 
 export default CheckoutSuccess;
